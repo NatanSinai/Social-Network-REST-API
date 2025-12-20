5c0cfb93bfdf1304b1379e2aff428eb0fbfb656c
shiramagr <shiramagr@gmail.com>
2025-12-20 16:50:26 +0200

    add get comments by comment id + by post id routes

--- src/comment/comment.route.ts
+++ src/comment/comment.route.ts
@@ -1,7 +1,7 @@
 import { commentModel, type Comment, type CommentDocument, type CreateCommentDTO } from '@comment';
 import { respondWithInvalidId, respondWithNotFound } from '@utils';
 import { Router, type Response } from 'express';
-import { isValidObjectId } from 'mongoose';
+import { isValidObjectId, type QueryFilter } from 'mongoose';
 
 const commentsRouter = Router();
 
@@ -19,4 +19,32 @@ commentsRouter.post<unknown, CommentDocument, CreateCommentDTO>('', async (reque
   const newComment = await commentModel.create(createCommentDTO);
 
   response.send(newComment);
+});
+
+/* Get All Comments By Post Id*/
+commentsRouter.get<unknown, CommentDocument[], unknown, { postId?: Comment['postId'] }>(
+  '',
+  async (request, response) => {
+    const { postId } = request.query;
+
+    if (!!postId && !isValidObjectId(postId)) return respondWithInvalidId(postId, response, 'post');
+
+    const commentsFilter = (postId ? { postId } : {}) satisfies QueryFilter<CommentDocument>;
+    const comments = await commentModel.find(commentsFilter);
+
+    response.send(comments);
+  },
+);
+
+/* Get Comment By ID */
+commentsRouter.get<{ commentId: Comment['_id'] }>('/:commentId', async (request, response) => {
+  const { commentId } = request.params;
+
+  if (!isValidObjectId(commentId)) return respondWithInvalidId(commentId, response, 'comment');
+
+  const comment = await commentModel.findById(commentId);
+
+  if (!comment) return respondWithNotFoundComment(commentId, response);
+
+  response.send(comment);
 });
