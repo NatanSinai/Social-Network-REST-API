1a5c47f0ce7184314cb75f4045005b26f46d24de
shiramagr <shiramagr@gmail.com>
2026-01-09 16:27:59 +0200

    adjust types

--- src/comment/comment.types.ts
+++ src/comment/comment.types.ts
@@ -1,8 +1,10 @@
 import type { DocumentMetadata, MakeOptional, Prettify } from '@utils';
-import type { HydratedDocument, ObjectId } from 'mongoose';
+import type { HydratedDocument, Types } from 'mongoose';
 
-export type Comment = Prettify<{ content: string; postId: ObjectId; senderId: ObjectId } & DocumentMetadata>;
+export type Comment = Prettify<
+  { content: string; postId: Types.ObjectId; senderId: Types.ObjectId } & DocumentMetadata
+>;
 export type CommentDocument = HydratedDocument<Comment>;
 
 export type CreateCommentDTO = MakeOptional<Pick<Comment, '_id' | 'content' | 'postId' | 'senderId'>, '_id'>;
-export type UpdateCommentDTO = Partial<Pick<Comment, 'content'>>;
+export type UpdateCommentDTO = Partial<Pick<Comment, 'content' | 'senderId'>>;
--- src/user/user.types.ts
+++ src/user/user.types.ts
@@ -1,8 +1,13 @@
 import type { DocumentMetadata, MakeOptional, Prettify } from '@utils';
 import type { HydratedDocument } from 'mongoose';
 
-export type User = Prettify<{ name: string; bio: string | null } & DocumentMetadata>;
+export type User = Prettify<
+  { name: string; email: string; isPrivate: boolean; postsCount: number; bio: string | null } & DocumentMetadata
+>;
 export type UserDocument = HydratedDocument<User>;
 
-export type CreateUserDTO = MakeOptional<Pick<User, '_id' | 'name' | 'bio'>, '_id'>;
-export type UpdateUserDTO = Partial<Pick<User, 'name' | 'bio'>>;
+export type CreateUserDTO = MakeOptional<
+  Pick<User, '_id' | 'name' | 'email' | 'isPrivate' | 'postsCount' | 'bio'>,
+  '_id'
+>;
+export type UpdateUserDTO = Partial<Pick<User, 'name' | 'email' | 'isPrivate' | 'postsCount' | 'bio'>>;
