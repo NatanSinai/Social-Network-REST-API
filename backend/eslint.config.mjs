2e2f1d05143cb16e85f733eca8a3b65b66d055e6
Natan Sinai <natanbriman@gmail.com>
2026-01-29 14:31:45 +0200

    refactor: create monorepo with backend and frontend (#14)
    * refactor: create monorepo with backend
    
    * feat: adding frontend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -10,6 +10,9 @@ export default tseslint.config({
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      projectService: true, tsconfigRootDir: import.meta.dirname,
+    },
   },
   rules: {
     '@typescript-eslint/no-empty-object-type': 'off',
---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Social network NASH frontend
+
+## Local development
+
+1. Install dependencies - execute `npm i`.
+2. Start development environment - execute `npm run dev`.
